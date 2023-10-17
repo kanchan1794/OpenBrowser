039e48a2860290731f8a6ccb94004eaee433c30c
kanchan1794 <46363163+kanchan1794@users.noreply.github.com>
2023-10-17 15:29:33 +0530

    Add files via upload

---
+++ LaunchBrowser.java
@@ -0,0 +1,39 @@
+package PageObjectClasses;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		//WebDriver driver = new ChromeDriver();
+        //driver.manage().window().maximize();
+        WebDriver driver;
+        //chromeoptions options = new chromeoptions();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        driver = new ChromeDriver(options);
+
+        // Launch Website
+        driver.get("https://www.google.com/");
+        Thread.sleep(2000);
+        System.out.println(driver.getTitle());
+        Assert.assertTrue("page title is not correct",driver.getTitle().equals("google"));
+        
+		/*
+		 * driver.findElement(By.id("login_username_input")).sendKeys(
+		 * "superadmin@myorganization");
+		 * driver.findElement(By.id("loging_password_input")).sendKeys("Admin@123");
+		 * driver.findElement(By.id("login_submit_button")).click();
+		 */
+
+
+		
+
+	}
+
+}
